23bce292ea6aed24544b76b75df19689afbb2cb0
XBWer <maxxbw1992@gmail.com>
2020-05-21 04:48:54 -0400

    improve timeutil

--- lib/utils/TimeUtil.java
+++ lib/utils/TimeUtil.java
@@ -3,7 +3,28 @@ package javaToolkit.lib.utils;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import org.eclipse.jdt.core.dom.ThisExpression;
+
 public class TimeUtil {
+
+	public long startTime;
+	public long endTime;
+
+	public TimeUtil() {
+		this.startTime = System.nanoTime();
+	}
+
+	public void setEndTime() {
+		this.endTime = System.nanoTime();
+	}
+
+	public int computeTimeCostInMinuts() {
+		return (int) ((this.endTime - this.startTime) / (1_000_000_000 * 60));
+	}
+
+	public int computeTimeCostInSecond() {
+		return (int) ((this.endTime - this.startTime) / 1_000_000_000);
+	}
 
 	public static void printCurTime() {
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
@@ -20,4 +41,5 @@ public class TimeUtil {
 	public static void main(String[] args) {
 		printCurTimewithMsg("test");
 	}
+
 }
